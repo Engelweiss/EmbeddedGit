9c52ccb2a13c6e2eef9dfcbe8ef73c5dddc3d42a
Engelweiss <seanhenry.vandyke@eagles.oc.edu>
2017-02-15 14:06:25 -0600

    Updated Code

--- Liquid_Crystal_Test_W_Date/Liquid_Crystal_Test_W_Date.ino
+++ Liquid_Crystal_Test_W_Date/Liquid_Crystal_Test_W_Date.ino
@@ -1,20 +1,30 @@
 #include <LiquidCrystal.h>
 #include <Process.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+
+#define ONE_WIRE_BUS 9
 
 LiquidCrystal lcd(12,11,5,4,3,2);
 int heatSwitchPin = 6;
-int filterSwitchPin = 7;
 int heatVal;
+int relayPin = 7;
+
+// Create a onewire instanace
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
 
 Process date;
 int lastSecond = -1;
 
 void setup() {
   // setup code
+  pinMode(relayPin, OUTPUT);
   lcd.begin(20,4);
   lcd.print("Pool Controller:");
   pinMode(heatSwitchPin, INPUT_PULLUP);
-  pinMode(filterSwitchPin, INPUT_PULLUP);
 
   Bridge.begin();
   if (!date.running()) {
@@ -22,6 +32,8 @@ void setup() {
     date.addParameter("+TIME: %T");
     date.run();
   }
+
+  sensors.begin();
 }
 
 void loop() {
@@ -30,11 +42,11 @@ void loop() {
   lcd.setCursor(0,2);
   if(heatVal) {
     lcd.print("Heater: ON ");
-    digitalWrite(13, HIGH);
+    digitalWrite(relayPin, HIGH);
   }
   else {
     lcd.print("Heater: OFF");
-    digitalWrite(13, LOW);
+    digitalWrite(relayPin, LOW);
   }
  
   while (date.available() > 0) {
@@ -43,6 +55,13 @@ void loop() {
     lcd.setCursor(0,1);
     lcd.print(timeString);
   }
+
+    // Temperature stuff
+    sensors.requestTemperatures(); // Tell the DS18B20 to get make a measurement
+    lcd.setCursor(0,3);
+    lcd.print("Temp: ");
+    lcd.print(sensors.getTempFByIndex(0),1);
+    lcd.print("F");
 
   if (!date.running()) {
     date.begin("date");
