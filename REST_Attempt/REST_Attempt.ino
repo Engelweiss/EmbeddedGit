cf1f45ae9a556881c033d55d57b67d367930e2a9
Engelweiss <seanhenry.vandyke@eagles.oc.edu>
2017-03-07 16:49:29 -0600

    Heater Filter Web Test Done
    dass right

--- REST_Attempt/REST_Attempt.ino
+++ REST_Attempt/REST_Attempt.ino
@@ -8,10 +8,14 @@
 #include <Stdio.h>
 #include <Arduino.h>
 #include <ds1620.h>
+#include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
 
 #define ONE_WIRE_BUS 9
 
 LiquidCrystal lcd(12,11,5,4,3,2);
+BridgeServer server;
 int heatSwitchPin = 6;
 int heatVal;
 int filterVal;
@@ -48,6 +52,17 @@ void setup() {
   sensors.begin();
 
   ds1620.config();
+
+  // Bridge startup
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  digitalWrite(13, HIGH);
+
+  // Listen for incoming connection only from localhost
+  // (no one from the external network could connect)
+  server.listenOnLocalhost();
+  server.begin();
 }
 
 // Loop through our 5 component modules
@@ -108,5 +123,55 @@ void loop() {
   lcd.print(temp);
   lcd.print("C");
 
+  //Bridge Process Module
+  BridgeClient client = server.accept();
+
+  // There is a new client?
+  if (client) {
+    // Process request
+    process(client);
+
+    // Close connection and free resources.
+    client.stop();
+  }
+
   delay(100);
 }
+
+void process(BridgeClient client) {
+  // read the command
+  String command = client.readStringUntil('/');
+
+  // is "digital" command?
+  if (command == "digital") {
+    digitalCommand(client);
+  }
+}
+
+void digitalCommand(BridgeClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/digital/13/1"
+  if (client.read() == '/') {
+    value = client.parseInt();
+    digitalWrite(pin, value);
+  } else {
+    value = digitalRead(pin);
+  }
+
+  // Send feedback to client
+  client.print(F("Pin D"));
+  client.print(pin);
+  client.print(F(" set to "));
+  client.println(value);
+
+  // Update datastore key with the current pin value
+  String key = "D";
+  key += pin;
+  Bridge.put(key, String(value));
+}
+
